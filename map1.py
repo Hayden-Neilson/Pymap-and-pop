e100fef52dbfa849dfa29a742c47c0715fdee3c5
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-07 19:27:28 -0600

    changed map type and added txt file

--- map1.py
+++ map1.py
@@ -2,10 +2,16 @@ import folium
 import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
-lat = list(data("LAT"))
-lon = list(data("LON"))
-
-for lt in in zip(lat, lon)
+lat = list(data["LON"])
+lon = list(data["LAT"])
 
 map = folium.Map(location=[74, -123], tiles="Stamen Terrain")
+
+theg = folium.FeatureGroup(name="my Map")
+
+for lt, ln in zip(lat, lon):
+    theg.add_child(folium.Marker(
+        location=[lt, ln], popup="hi iam  Marker", icon=folium.Icon(color='red')))
+
+map.add_child(theg)
 map.save("Map.html")
