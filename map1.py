8e26c4fd6d7c18ce45acd3b6712771c677a70707
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-08 18:02:38 -0600

    added styles and a heights var to earch item in the list

--- map1.py
+++ map1.py
@@ -4,14 +4,20 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LON"])
 lon = list(data["LAT"])
+elev = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
 
 map = folium.Map(location=[74, -123], tiles="Stamen Terrain")
 
 theg = folium.FeatureGroup(name="my Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
     theg.add_child(folium.Marker(
-        location=[lt, ln], popup="hi iam  Marker", icon=folium.Icon(color='red')))
+        location=[lt, ln], popup=str(el) + "m", icon=folium.Icon(color='red')))
 
 map.add_child(theg)
 map.save("Map.html")
