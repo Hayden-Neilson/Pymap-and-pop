ad40caae5a1aa651c3d8d0cd044805b4966e03f2
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-09 21:13:41 -0600

    added finishing touches

--- map1.py
+++ map1.py
@@ -17,17 +17,23 @@ def color_producer(elevation):
         return "red"
 
 
-map = folium.Map(location=[74, -123], tiles="Stamen Terrain")
+map = folium.Map(location=[13.067439, 80.237617],
+                 Zoom_start=6, titles="Stamen Terrain")
 
-theg = folium.FeatureGroup(name="my Map")
+theg = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
     theg.add_child(folium.CircleMarker(
         location=[lt, ln], popup=str(el) + "m", fill_color=color_producer(el), radius=8,
         color="grey", fill_opacity=0.7))
 
-theg.add_child(folium.GeoJson(
-    data=(open('world.json', 'r', encoding=('utf-8-sig').read()))))
+theggg = folium.FeatureGroup(name="Population")
+
+theg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                              style_function=lambda x: {'fillColor': 'yellow'
+                                                        if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(theg)
+map.add_child(theggg)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
